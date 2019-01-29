1b9b04ca9559306d99d59f746b1b2903824eb3f2
xgggggggg <xiaggg@qq.com>
2019-01-28 19:13:15 -0800

    TimeStamp add more methods

--- include/TimeStamp.h
+++ include/TimeStamp.h
@@ -16,8 +16,26 @@ class TimeStamp {
 public:
 	TimeStamp();
 	explicit TimeStamp(int64_t microseconds);
+	int64_t getMicroSeconds();
+	inline bool operator<(TimeStamp rhs) {
+		return this->getMicroSeconds() < rhs.getMicroSeconds();
+	}
+	inline bool operator==(TimeStamp rhs) {
+		return this->getMicroSeconds() == rhs.getMicroSeconds();
+	}
+	inline bool operator>(TimeStamp rhs) {
+		return this->getMicroSeconds() > rhs.getMicroSeconds();
+	}
+	inline bool operator<=(TimeStamp rhs) {
+		return !(*this > rhs);
+	}
+	inline bool operator>=(TimeStamp rhs) {
+		return !(*this < rhs);
+	}
 
 	static TimeStamp now();
+	static TimeStamp invalid();
+	static TimeStamp addTime(TimeStamp, double);
 
 private:
 	const static int kMicroSecPerSec = 1000 * 1000;
--- src/TimeStamp.cc
+++ src/TimeStamp.cc
@@ -18,10 +18,23 @@ TimeStamp::TimeStamp(int64_t microseconds) {
 	m_mircoseconds = microseconds;
 }
 
+int64_t TimeStamp::getMicroSeconds() {
+	return m_mircoseconds;
+}
+
 TimeStamp TimeStamp::now() {
 	struct timeval tv;
 	gettimeofday(&tv, nullptr);
 	return TimeStamp(tv.tv_sec * kMicroSecPerSec + tv.tv_usec);
+}
+
+TimeStamp TimeStamp::invalid() {
+	return TimeStamp();
+}
+
+TimeStamp TimeStamp::addTime(TimeStamp timestamp, double seconds) {
+	int64_t delta = static_cast<int64_t>(seconds * kMicroSecPerSec);
+	return TimeStamp(timestamp.getMicroSeconds() + delta);
 }
 
 }
