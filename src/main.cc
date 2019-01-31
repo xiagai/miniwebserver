866c5f4b2821b2fb60a7a479c951bf5b6b466462
xgggggggg <xiaggg@qq.com>
2019-01-30 20:38:30 -0800

    main添加了对定时器的测试

--- src/main.cc
+++ src/main.cc
@@ -47,32 +47,44 @@
 //
 //}
 
-miniws::EventLoop *g_loop;
-void timeout() {
-	printf("Timeout!\n");
-	g_loop->quit();
+// miniws::EventLoop *g_loop;
+// void timeout() {
+// 	printf("Timeout!\n");
+// 	g_loop->quit();
+// }
+
+// void test3() {
+// 	miniws::EventLoop loop;
+// 	g_loop = &loop;
+
+// 	int timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK |TFD_CLOEXEC);
+// 	miniws::Channel channel(&loop, timerfd);
+// 	channel.setReadCallback(timeout);
+// 	channel.enableReading();
+
+// 	itimerspec howlong;
+// 	bzero(&howlong, sizeof howlong);
+// 	howlong.it_value.tv_sec = 5;
+// 	::timerfd_settime(timerfd, 0, &howlong, NULL);
+
+// 	loop.loop();
+// 	::close(timerfd);
+// }
+
+void f1() {
+    printf("f1\n");
 }
 
-void test3() {
-	miniws::EventLoop loop;
-	g_loop = &loop;
-
-	int timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK |TFD_CLOEXEC);
-	miniws::Channel channel(&loop, timerfd);
-	channel.setReadCallback(timeout);
-	channel.enableReading();
-
-	itimerspec howlong;
-	bzero(&howlong, sizeof howlong);
-	howlong.it_value.tv_sec = 5;
-	::timerfd_settime(timerfd, 0, &howlong, NULL);
-
-	loop.loop();
-	::close(timerfd);
+void test4() {
+    miniws::EventLoop loop;
+    
+    loop.runAfter(60, f1);
+    //loop.loop();
 }
+
 
 int main() {
-	test3();
+    test4();
 }
 
 
